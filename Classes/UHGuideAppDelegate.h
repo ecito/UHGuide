29b1b97cc21bd2d106c19c049ae2703993dc1a58
Andre Navarro <andre.navarro@gmail.com>
2010-09-24 08:25:00 -0500

    baseline uhguide project

---
+++ Classes/UHGuideAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  UHGuideAppDelegate.h
+//  UHGuide
+//
+//  Created by Andre Navarro on 9/24/10.
+//  Copyright Squirrel Hero 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UHGuideAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
